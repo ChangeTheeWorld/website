f0b9cfb51e0e3689f93946ad99e1c6918943d767
AgentCreator <320320ua@gmail.com>
2024-07-03 21:07:29 +0300

    init

---
+++ blog/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.utils import timezone
+
+
+# Create your models here.
+class BlogPost(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.CharField(max_length=200)
+    description = models.TextField(max_length=20, default="no description")
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    blog_post = models.ForeignKey(BlogPost, on_delete=models.CASCADE)
+    author = models.CharField(max_length=200, default="unknown")
+    content = models.TextField()
+    likes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.content
---
+++ blog/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+
+from .models import BlogPost, Comment
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+# Create your views here.
+def index(req):
+    content = {"posts": BlogPost.objects.all()}
+    return render(req, "blog/index.html", content)
+
+
+def blog(req, blog_id):
+    return render(req, "blog/blog.html", {"post": BlogPost.objects.get(id=blog_id)})
+
+
+def comment(req, blog_id):
+    post = BlogPost.objects.get(id=blog_id)
+    blog_comment = req.POST["comment"]
+    post.comment_set.create(content=blog_comment)
+    post.save()
+    return HttpResponseRedirect(reverse('blog', args=[post.id]))
+
+
+def comment_like(req, comment_id):
+    comm = Comment.objects.get(id=comment_id)
+    post = comm.blog_post
+    print(req.POST)
+    val = 0
+    if "like" in req.POST:
+        val = 1
+    elif "dislike" in req.POST:
+        val = -1
+
+    comm.likes += val
+    comm.save()
+    return HttpResponseRedirect(reverse('blog', args=[post.id]))
