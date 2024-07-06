39a18200f062d50ca0c23d9e6357951f66bede70
AgentCreator <83863534+AgentCreator@users.noreply.github.com>
2024-07-06 20:38:54 +0300

    removed the unnecesary comment.

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,7 @@ from .models import BlogPost, Comment
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
-# Create your views here.
+
 def index(req):
     content = {"posts": BlogPost.objects.all()}
     return render(req, "blog/index.html", content)
